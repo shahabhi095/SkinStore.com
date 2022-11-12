13f3271f3a7b71b9605f82674cbb2b884d5071a8
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2022-11-13 00:34:27 +0530

    order summary added

--- checkout.js
+++ checkout.js
@@ -39,8 +39,9 @@ let append_orderr_summary = (data) => {
     quantity.innerText = `Quantity:${1}`;
 
     let price = document.createElement("p");
-    price.innerText = `$ ${el.price}`;
-    total += el.price;
+    price.innerText = el.price;
+
+    total += Number(el.price);
 
     img_div.append(img);
     descripton_div.append(name, quantity, price);
@@ -49,7 +50,7 @@ let append_orderr_summary = (data) => {
   });
   other_order_detail();
   // console.log(count);
-  console.log(total);
+  // console.log(total);
 };
 
 let other_order_detail = () => {
@@ -60,12 +61,15 @@ let other_order_detail = () => {
   document.getElementById("order_pay").innerText = `$${total + 15}`;
 };
 
-// popup
+// popup;
+
 let popup = () => {
   let checkout_btn = document.getElementById("checkout_submit");
   checkout_btn.onclick = () => {
     // console.log("hi");
-    window.location.replace = "checkout.html";
+    GetAddress();
+
+    // window.location.replace = "checkout.html";
     let popup = document.getElementById("popup");
     popup.classList.add("open_popup");
   };
@@ -79,3 +83,61 @@ let close_popup = () => {
   };
 };
 close_popup();
+
+// address data to object
+let GetAddress = () => {
+  let country = document.getElementById("country").value;
+  let f = document.getElementById("full_name").value;
+  let z = document.getElementById("zipcode").value;
+  let cn = document.getElementById("company_name").value;
+  let add1 = document.getElementById("address1").value;
+  let add2 = document.getElementById("address2").value;
+  let hn = document.getElementById("house_no").value;
+  let c = document.getElementById("city").value;
+  let s = document.getElementById("state").value;
+  let mn = document.getElementById("mobile_no").value;
+
+  let x = Math.floor(Math.random() * 100001);
+
+  if (
+    country == "" ||
+    f == "" ||
+    z == "" ||
+    add1 == "" ||
+    add2 == "" ||
+    hn == "" ||
+    c == "" ||
+    s == "" ||
+    mn == ""
+  ) {
+    alert("Please Fill required areas");
+    // window.location.reload;
+  } else {
+    let obj = new address(country, f, z, cn, add1, add2, hn, c, s, mn, x);
+    send(obj);
+  }
+};
+
+function address(country, f, z, cn, add1, add2, hn, c, s, mn, x) {
+  this.country = country;
+  this.fullname = f;
+  this.zipcode = z;
+  this.companyname = cn;
+  this.address1 = add1;
+  this.address2 = add2;
+  this.houseno = hn;
+  this.city = c;
+  this.state = s;
+  this.mobileno = mn;
+  this.orderno = x;
+}
+
+// data send to server
+
+let send = async (obj) => {
+  let res = await fetch("http://localhost:3000/address", {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: { "Content-Type": "application/json" },
+  });
+};
